19a5cac275d542e9f7e4d5cae056d92fd1743d9e
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2018-06-10 19:26:43 -0300

    Añadidas palabras clave, reglas y comentarios en métodos.

--- ChatBotIA/src/logica/Agente.java
+++ ChatBotIA/src/logica/Agente.java
@@ -42,36 +42,77 @@ public class Agente {
         
         palabras = new HashMap <String,String>();
         
-        palabras.put("Parent", "Parent");
-        palabras.put("Parents", "Parent");
-        palabras.put("Dad", "Parent");
-        palabras.put("Mom", "Parent");
-        palabras.put("Father", "Parent");
-        palabras.put("Mother", "Parent");
-        palabras.put("Daddy", "Parent");
-        palabras.put("Mommy", "Parent");
-        palabras.put("Family Member", "Parent");
-        palabras.put("What", "What");
-        palabras.put("What’s", "What");
-        palabras.put("Which", "What");
-        palabras.put("How", "How");
-        palabras.put("How's", "How");
+        //Ubicación espacio-temporal
+        palabras.put("After", "After");
+        palabras.put("Before", "Before");
+        palabras.put("Be", "Be");
+        palabras.put("Being", "Be");
+        palabras.put("Go", "Go");
+        palabras.put("Going", "Go");
+        palabras.put("Holiday", "Place");
+        palabras.put("Holidays", "Place");
+        palabras.put("Place", "Place");
+        palabras.put("Vacation", "Place");
+        palabras.put("Where", "Place");
+        
+        //Expresiones de voluntad
+        palabras.put("Want", "Want");
+        palabras.put("Would", "Would");
+        palabras.put("Have", "Have");
+        palabras.put("Had", "Have");
+        
+        //Actividades
+        palabras.put("Hobby", "Hobby");
+        palabras.put("Build", "Build");
+        palabras.put("Builded", "Build");
+        
+        //Referencias a personas o al niño
         palabras.put("Who", "Who");
-        palabras.put("Who's", "How");
         palabras.put("You", "You");
         palabras.put("Your", "You");
+        
+        //Family/Friends/Parents related
+        palabras.put("Family", "Family");
+        palabras.put("Dad", "Parent");
+        palabras.put("Daddy", "Parent");
+        palabras.put("Family", "Parent");
+        palabras.put("Family Member", "Parent");
+        palabras.put("Father", "Parent");
+        palabras.put("Mom", "Parent");
+        palabras.put("Mommy", "Parent");
+        palabras.put("Mother", "Parent");
+        palabras.put("Parent", "Parent");
+        palabras.put("Parents", "Parent");
+        
+        //Home-related
+        palabras.put("Home", "Home");
+        palabras.put("House", "Home");
+        palabras.put("Household", "Home");
+        palabras.put("Apartment", "Home");
+        palabras.put("Place", "Home");
+        
+        //How-related
+        palabras.put("How", "How");
+        palabras.put("How's", "How");
+        palabras.put("Who's", "How");
+        
+        //Job-related
+        palabras.put("Job", "Job");
+        palabras.put("Jobs", "Job");
+        palabras.put("Activity", "Job");
+        palabras.put("Business", "Job");
+        palabras.put("Career", "Job");
+        palabras.put("Profession", "Job");
+        palabras.put("Work", "Job");
+        palabras.put("Occupation", "Job");
+        
+        //Name-related
         palabras.put("Name", "Name");
         palabras.put("First Name", "Name");
         palabras.put("Middle Name", "Name");
         palabras.put("Last Name", "Name");
-        palabras.put("Friend", "Friend");
-        palabras.put("Buddy", "Friend");
-        palabras.put("Family", "Family");
-        palabras.put("After", "After");
-        palabras.put("Old", "Old");
-        palabras.put("Age", "Old");
-        palabras.put("Be", "Be");
-        palabras.put("Being", "Be");
+        
+        //Number-related
         palabras.put("Number", "Number");
         palabras.put("One", "Number");
         palabras.put("Two", "Number");
@@ -83,12 +124,36 @@ public class Agente {
         palabras.put("Eight", "Number");
         palabras.put("Nine", "Number");
         palabras.put("Ten", "Number");
-        palabras.put("Born", "Born");
-        palabras.put("Live", "Live");
-        palabras.put("School", "School");
+        
+        //Old (age) related
+        palabras.put("Old", "Old");
+        palabras.put("Age", "Old");
+       
+        //School-related
         palabras.put("Kindergarten", "School");
         palabras.put("Middle School", "School");
         palabras.put("High School", "School");
+        
+        //Town-related
+        palabras.put("Town", "Town");
+        palabras.put("City", "Town");
+        palabras.put("Province", "Town");
+
+        //What-related
+        palabras.put("What", "What");
+        palabras.put("What’s", "What");
+        palabras.put("Which", "What");
+       
+
+        palabras.put("Friend", "Friend");
+        palabras.put("Buddy", "Friend");
+
+
+
+        palabras.put("Born", "Born");
+        palabras.put("Live", "Live");
+        palabras.put("School", "School");
+
         palabras.put("Grade", "Grade");
         palabras.put("Teacher", "Teacher");
         palabras.put("Proffesor", "Teacher");
@@ -113,38 +178,29 @@ public class Agente {
         palabras.put("Choker", "Necklace");
         palabras.put("Pendant", "Necklace");
         palabras.put("Strand", "Necklace");
-        palabras.put("Job", "Job");
-        palabras.put("Activity", "Job");
-        palabras.put("Business", "Job");
-        palabras.put("Career", "Job");
-        palabras.put("Profession", "Job");
-        palabras.put("Work", "Job");
-        palabras.put("Occupation", "Job");
-        palabras.put("Home", "Home");
-        palabras.put("House", "Home");
-        palabras.put("Household", "Home");
-        palabras.put("Apartment", "Home");
-        palabras.put("Place", "Home");
+
+        palabras.put("Sex", "Sex");
+        palabras.put("Show", "Show");
+        
         palabras.put("Look", "Look");
         palabras.put("Tell", "Tell");
         palabras.put("More", "More");
-        palabras.put("Home", "Home");
+
+        //Is-related
         palabras.put("Is", "Is");
         palabras.put("Was", "Is");
         palabras.put("Were", "Is");
+        
         palabras.put("Decorated", "Decorated");
         palabras.put("Adorned", "Decorated");
+        
         palabras.put("Dream", "Dream");
+        
         palabras.put("Help", "Help");
         palabras.put("Helped", "Help");
-        palabras.put("Build", "Build");
-        palabras.put("Builded", "Build");
-        palabras.put("Have", "Have");
-        palabras.put("Had", "Have");
-        palabras.put("Vacation", "Vacation");
-        palabras.put("Town", "Town");
-        palabras.put("City", "Town");
-        palabras.put("Province", "Town");
+
+
+
         palabras.put("License", "License");
         palabras.put("Driver's License", "License");
         palabras.put("Drive", "Drive");
@@ -157,13 +213,14 @@ public class Agente {
         palabras.put("Gathering", "Collecting");
         palabras.put("Book", "Book");
         palabras.put("Books", "Book");
-        palabras.put("Hobby", "Hobby");
 
-        
+
     }
-    
-    //Se inicializa la lista de reglas
-    //Formato: new Regla(id, lista de palabras claves, lista de respuestas, prioridad);
+   
+    /**
+     * Se inicializa la lista de reglas
+     * Formato: new Regla(id, lista de palabras claves, lista de respuestas, prioridad);
+     */
     public void inicializarReglas(){
         
         reglas = new ArrayList<Regla>();
@@ -179,10 +236,22 @@ public class Agente {
         reglas.add(new Regla(3, new ArrayList<String>(Arrays.asList("After", "Hobby")), new ArrayList<Respuesta>(Arrays.asList(new Decir("After y hobby 2"))), 2));
         reglas.add(new Regla(4, new ArrayList<String>(Arrays.asList("After", "Hobby")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres(), new AdvertirNoResponder())), 2));
         
-        //TODO: Definir reglas
+        //Trabajo de los padres
+        reglas.add(new Regla(5, new ArrayList<String>(Arrays.asList("Parent", "Job")), new ArrayList<Respuesta>(Arrays.asList(new AdvertirNoResponder())), 2));
+        reglas.add(new Regla(6, new ArrayList<String>(Arrays.asList("Parent", "Job")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres())), 1));
+        
+        //Dónde te vas después de...
+        reglas.add(new Regla(7, new ArrayList<String>(Arrays.asList("Going", "Where")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres())), 2));
+        reglas.add(new Regla(8, new ArrayList<String>(Arrays.asList("Going", "Where", "After")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres())), 2));
+        reglas.add(new Regla(9, new ArrayList<String>(Arrays.asList("Going", "After", "School")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres())), 2));
+        reglas.add(new Regla(10, new ArrayList<String>(Arrays.asList("After", "School")), new ArrayList<Respuesta>(Arrays.asList(new LlamarPadres())), 2));
+
+        //Definir más reglas
     }
     
-    //Se inicializa la lista de criterios a aplicarse en el orden que se desea que se apliquen (los criterios se anidan)
+    /**
+     *     Inicializa la lista de criterios a aplicarse en el orden que se desea que se apliquen (los criterios se anidan)
+     */
     public void inicializarCriterios(){
         criterios = new LinkedList<Criterio>();
         
--- ChatBotIA/src/logica/PreProcesador.java
+++ ChatBotIA/src/logica/PreProcesador.java
@@ -9,12 +9,12 @@ import java.util.regex.Pattern;
 
 public class PreProcesador {
     
-   public static List<String> preprocesarEntrada(Agente agente, Map <String,String> palabras, String frase){
+   public static List<String> preprocesarEntrada(Agente agente, Map <String,String> palabras, String fraseOriginal){
         List <String> pc = new ArrayList<String>();
         //Se convierten las keys del mapa de sinonimos - palabras claves obtenido por parametro a minúsculas
         Map<String, String> palabrasClaves = convertirPalabrasClavesAMinusculas(palabras);
         //Se obtiene una lista de palabras a partir de la frase ingresada por parámetro
-        List<String> palabrasFrase = obtenerPalabras(frase);
+        List<String> palabrasFrase = obtenerPalabras(fraseOriginal);
         //Se obtienen la lista de palabras claves ingresadas usando el mapa sinonimos - palabras claves y la lista de palabras obtenidas de la frase de entrada
         for (String palabra : palabrasFrase){
             if(palabrasClaves.containsKey(palabra.toLowerCase())){
@@ -23,7 +23,11 @@ public class PreProcesador {
         }
         return pc;
     }
-   
+   /**
+    * Devuelve en lowercase un mapa las palabras clave del mapa que es pasado como parámetro
+    * @param palabrasClaves Map<String, String>
+    * @return resultado  Map<String, String>
+    */
    public static Map<String, String> convertirPalabrasClavesAMinusculas(Map<String, String> palabrasClaves){
        Map<String, String> resultado = new HashMap<String, String>();
        for(String clave : palabrasClaves.keySet()){
@@ -32,6 +36,11 @@ public class PreProcesador {
        return resultado;
    }
    
+   /**
+    * Devuelve la lista de palabras componentes de la frase inicial pasada como parámetro
+    * @param frase List<String>
+    * @return palabras String
+    */
    public static List<String> obtenerPalabras(String frase){
        List<String> palabras = new ArrayList<String>();
        String palabraAux;
